bcd86cfd9d0242fc256537fa0273ea15e7a75dee
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-19 08:17:47 -0800

    main: add correct

--- pages/api/question.js
+++ pages/api/question.js
@@ -1,15 +1,21 @@
+global.crypto = require('crypto')
+
 export default function handler(req, res) {
+  const text = 'Hello World!!'
+  const answers = [
+    {
+      text: 'Dude'
+    },
+    {
+      text: 'Stoked'
+    }
+  ]
+  const i = 1
+  const correct = crypto.createHash('sha512').update(text + answers[i].text).digest('hex')
   const data = {
-    text: "Hello World",
-    answers: [
-      {
-        text: "Dude"
-      },
-      {
-        text: "Stoked"
-      }
-    ],
-    correct: null
+    text,
+    answers,
+    correct,
   }
   res.status(200).json(data)
 }
