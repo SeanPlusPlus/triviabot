9234841281a4e6384524e343f70ee242a51caf12
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-19 12:56:27 -0800

    main: generate prompt

--- pages/api/question.js
+++ pages/api/question.js
@@ -1,6 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+
 global.crypto = require('crypto')
 
-export default function handler(req, res) {
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+const prompt = 'Write a Disney trivia question, provide four responses, three incorrect, and one correct. And then tell me the correct answer.'
+
+const generateQuestion = async (req, res) => {
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const output = baseCompletion.data.choices.pop();
+
   const text = new Date() + ''
   const answers = [
     {
@@ -16,6 +36,9 @@ export default function handler(req, res) {
     text,
     answers,
     correct,
+    output,
   }
   res.status(200).json(data)
 }
+
+export default generateQuestion
