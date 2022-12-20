459c25fe833cc382ed2db096dbe63c73f46d6a59
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-20 08:14:15 -0800

    main: orderby leaderboard

--- pages/api/leaderboard.js
+++ pages/api/leaderboard.js
@@ -1,6 +1,10 @@
+import _orderBy from 'lodash/orderBy'
 import getLeaderboard from "../../utils/getLeaderboard"
 
 export default async function handler(req, res) {
   const data = await getLeaderboard()
-  res.status(200).json(data)
+  const leaderboard = _orderBy(data, (item) => (
+    item.json.streak
+  ), ['desc'])
+  res.status(200).json(leaderboard)
 }
