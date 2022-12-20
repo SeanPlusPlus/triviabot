6ea424ad70e18993daa06d7ae346e9cc172af35a
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-19 18:21:22 -0800

    main: more brand integration

--- components/question.js
+++ components/question.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import { sha512 } from '../utils/crypto'
+import Image from 'next/image'
 
 const Question = () => {
   const [flag, setFlag] = useState(false)
@@ -40,9 +41,19 @@ const Question = () => {
 
   return (
     <div className="mx-10 md:mx-24 lg:mx-48 xl:mx-96 mb-4 px-2 py-2 border rounded bg-base-100">
-      <p className="text-left text-lg pb-1 pl-1">
-        {question.text}
-      </p>
+      <div className="flex">
+        <div className="mr-1">
+          <Image
+            className="rounded"
+            src={`/brands/${question.prompt.categorySlug}.png`}
+            height='35'
+            width='35'
+          />
+        </div>
+        <p className="text-left text-sm pb-1 pl-1 md:pt-2">
+          {question.text}
+        </p>
+      </div>
       {question.answers.map((a) => (
         <div key={a.text} className="form-control">
           <label className="label cursor-pointer hover:bg-base-300 rounded" onClick={() => handleClick(question.text, a)}>
