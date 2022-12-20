74831c62cae3a00f7e87061e5e43f717a8dfe016
Sean Stephenson <seanmichaelstephenson@gmail.com>
2022-12-19 19:47:49 -0800

    supplement

--- utils/prompt.js
+++ utils/prompt.js
@@ -4,29 +4,46 @@ const getPrompt = () => {
   const categories = [
     {
       name: 'Disney',
+      supplement: [
+        'History',
+      ]
     },
     {
       name: 'Pixar',
+      supplement: [
+        'Cars',
+      ]
     },
     {
       name: 'Marvel',
+      supplement: [
+        'Avengers',
+      ]
     },
     {
       name: 'Star Wars',
+      supplement: [
+        'Episode IV',
+      ]
     },
     {
       name: 'National Geographic',
+      supplement: [
+        'space',
+      ]
     },
   ]
 
   const category = _sample(categories)
   const categorySlug = category.name.split(' ').join('').toLocaleLowerCase()
+  const supplement = _sample(category.supplement)
 
-  const text = `Write a ${category.name} trivia question, provide four responses, three incorrect, and one correct. And then tell me the correct answer.`
+  const text = `Write a ${category.name} ${category.supplement} trivia question, provide four responses, three incorrect, and one correct. And then tell me the correct answer.`
 
   return {
     categorySlug,
     category,
+    supplement,
     text,
   }
 }
