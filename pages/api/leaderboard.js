46b8b492aa208c950b91f81d5190b41e722d0e0a
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-20 08:19:31 -0800

    main: clean data

--- pages/api/leaderboard.js
+++ pages/api/leaderboard.js
@@ -1,10 +1,14 @@
 import _orderBy from 'lodash/orderBy'
+import _get from 'lodash/get'
 import getLeaderboard from "../../utils/getLeaderboard"
 
 export default async function handler(req, res) {
   const data = await getLeaderboard()
   const leaderboard = _orderBy(data, (item) => (
     item.json.streak
-  ), ['desc']).filter((item) => (item.json.streak))
+  ), ['desc']).filter((item) => {
+    const streak = _get(item, 'json.streak')
+    return Number.isInteger(streak)
+  })
   res.status(200).json(leaderboard)
 }
