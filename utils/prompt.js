2a0869187694fad18681d56ce403d9bf0493cf88
Sean Stephenson <seanmichaelstephenson@gmail.com>
2022-12-19 19:43:06 -0800

    refactor cats

--- utils/prompt.js
+++ utils/prompt.js
@@ -2,16 +2,27 @@ import _sample from 'lodash/sample'
 
 const getPrompt = () => {
   const categories = [
-    'Disney',
-    'Pixar',
-    'Marvel',
-    'Star Wars',
-    'National Geographic',
+    {
+      name: 'Disney',
+    },
+    {
+      name: 'Pixar',
+    },
+    {
+      name: 'Marvel',
+    },
+    {
+      name: 'Star Wars',
+    },
+    {
+      name: 'National Geographic',
+    },
   ]
 
   const category = _sample(categories)
-  const categorySlug = category.split(' ').join('').toLocaleLowerCase()
-  const text = `Write a ${category} trivia question, provide four responses, three incorrect, and one correct. And then tell me the correct answer.`
+  const categorySlug = category.name.split(' ').join('').toLocaleLowerCase()
+
+  const text = `Write a ${category.name} trivia question, provide four responses, three incorrect, and one correct. And then tell me the correct answer.`
 
   return {
     categorySlug,
