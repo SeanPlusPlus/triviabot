57b864db189b36a68d2b4ac23f78e6e72888a6e1
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-19 21:15:41 -0800

    main: update style

--- utils/slack.js
+++ utils/slack.js
@@ -2,111 +2,111 @@ const getPayload = (data, prompt) => {
 	const { text, answers, answer, error } = data
 	if (error) {
 		return {
-			"blocks": [
+			blocks: [
 				{
-					"type": "header",
-					"text": {
-						"type": "plain_text",
-						"text": " :warning: GPT3 Question Generated but not parsed",
-						"emoji": true
+					type: 'header',
+					text: {
+						type: 'plain_text',
+						text: ':warning: GPT3 Question Generated but not parsed',
+						emoji: true
 					}
 				},
 				{
-					"type": "divider"
+					type: 'divider'
 				},
 				{
-					"type": "section",
-					"text": {
-						"type": "mrkdwn",
-						"text": prompt.text
+					type: 'section',
+					text: {
+						type: 'mrkdwn',
+						text: prompt.text
 					}
 				},
 				{
-					"type": "divider"
+					type: 'divider'
 				},
 				{
-					"type": "section",
-					"text": {
-						"type": "mrkdwn",
-						"text": data.gpt3output
+					type: 'section',
+					text: {
+						type: 'mrkdwn',
+						text: data.gpt3output
 					}
 				},
 			]
 		}
 	}
 	return {
-		"blocks": [
+		blocks: [
 			{
-				"type": "header",
-				"text": {
-					"type": "plain_text",
-					"text": ":white_check_mark: GPT3 Question Generated",
-					"emoji": true
+				type: 'header',
+				text: {
+					type: 'plain_text',
+					text: ':white_check_mark: GPT3 Question Generated',
+					emoji: true
 				}
 			},
 			{
-				"type": "divider"
+				type: 'divider'
 			},
 			{
-				"type": "section",
-				"fields": [
+				type: 'section',
+				fields: [
 					{
-						"type": "mrkdwn",
-						"text": "*Text:*\n" + text
+						type: 'mrkdwn',
+						text: '*Text:*\n' + text
 					},
 					{
-						"type": "mrkdwn",
-						"text": "*Answer:*\n" + answer 
+						type: 'mrkdwn',
+						text: '*Answer:*\n' + answer 
 					},
 				]
 			},
 			{
-				"type": "divider"
+				type: 'divider'
 			},
 			{
-				"type": "section",
-				"fields": [
+				type: 'section',
+				fields: [
 					{
-						"type": "mrkdwn",
-						"text": answers[0].text
+						type: 'mrkdwn',
+						text: answers[0].text
 					},
 					{
-						"type": "mrkdwn",
-						"text": answers[1].text
+						type: 'mrkdwn',
+						text: answers[1].text
 					}
 				]
 			},
 			{
-				"type": "section",
-				"fields": [
+				type: 'section',
+				fields: [
 					{
-						"type": "mrkdwn",
-						"text": answers[2].text
+						type: 'mrkdwn',
+						text: answers[2].text
 					},
 					{
-						"type": "mrkdwn",
-						"text": answers[3].text
+						type: 'mrkdwn',
+						text: answers[3].text
 					}
 				]
 			},
 			{
-				"type": "divider"
+				type: 'divider'
 			},
 			{
-				"type": "section",
-				"fields": [
+				type: 'section',
+				fields: [
 					{
-						"type": "mrkdwn",
-						"text": "*Category:*\n" + prompt.category.name
+						type: 'mrkdwn',
+						text: '*Category:*\n' + prompt.category.name
 					},
 					{
-						"type": "mrkdwn",
-						"text": "*Supplement:*\n" + prompt.supplement
+						type: 'mrkdwn',
+						text: '*Supplement:*\n' + prompt.supplement
 					}
 				]
 			},
 			{
-				"type": "divider"
+				type: 'divider'
 			},
 		]
 	}
