632e88197bad3360247e8d57b3012ab30ab3aa50
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-20 13:27:08 -0800

    main: refactor cats

---
+++ utils/categories.js
@@ -0,0 +1,84 @@
+const categories = [
+  {
+    name: 'Disney',
+    supplement: [
+      'History',
+      'Themes Parks',
+      'Disneyland',
+      'Disney World',
+      'Animation',
+      'Movies',
+      'Moana',
+      'Big Hero 6',
+      'Lion King',
+      'Aladin',
+      'Frozen',
+    ]
+  },
+  {
+    name: 'Pixar',
+    supplement: [
+      'Cars',
+      'Toy Story',
+      'Monsters',
+      'History',
+      'Inside Out',
+      'Finding Nemo',
+      'Brave',
+      'History',
+    ]
+  },
+  {
+    name: 'Marvel',
+    supplement: [
+      'Avengers',
+      'Comics',
+      'Captain America',
+      'Black Panther',
+      'Iron Man',
+      'X-Men',
+      'Wolverine',
+      'Captain Marvel',
+    ]
+  },
+  {
+    name: 'Star Wars',
+    supplement: [
+      'Episode IV',
+      'Episode V',
+      'Episode VI',
+      'The Force Awakens',
+      'Prequels',
+      'Skywalker',
+      'The Empire Strikes Back',
+      'Clone Wars',
+    ]
+  },
+  {
+    name: 'National Geographic',
+    supplement: [
+      'space',
+      'geography',
+      'history',
+      'culture',
+      'Europe',
+      'Africa',
+      'Asia',
+      'history',
+    ]
+  },
+  {
+    name: 'ESPN',
+    supplement: [
+      'NFL',
+      'MLB',
+      'NBA',
+      'College Football',
+      'WNBA',
+      'Golf',
+      'Soccer',
+    ]
+  },
+]
+
+export default categories
