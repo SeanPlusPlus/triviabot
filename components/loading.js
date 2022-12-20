b83432537b7147555c92451fcdeb58d99215ce1d
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-20 12:54:42 -0800

    main: loading

--- components/loading.js
+++ components/loading.js
@@ -6,16 +6,14 @@ const Loading = () => {
     loading
   } = useContext(GlobalContext)
 
-  if (!loading) {
-    return <></>
-  }
+  // if (!loading) {
+  //   return <></>
+  // }
 
   return (
     <div className="mx-10 md:mx-24 lg:mx-48 xl:mx-96 mb-4 px-20 py-20 border rounded bg-base-100">
-      <div className="lds-loader">
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="flex items-center justify-center">
+        <div className="loader"></div>
       </div>
     </div>
   )
