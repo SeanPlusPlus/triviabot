144a57dd07e7cbd515b248cd5e400539f8a315c8
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-20 13:27:02 -0800

    main: more supplement

--- utils/prompt.js
+++ utils/prompt.js
@@ -1,63 +1,7 @@
 import _sample from 'lodash/sample'
+import categories from './categories'
 
 const getPrompt = () => {
-  const categories = [
-    {
-      name: 'Disney',
-      supplement: [
-        'History',
-        'Themes Parks',
-        'Animation',
-        'Movies',
-      ]
-    },
-    {
-      name: 'Pixar',
-      supplement: [
-        'Cars',
-        'Toy Story',
-        'Monsters',
-        'History',
-      ]
-    },
-    {
-      name: 'Marvel',
-      supplement: [
-        'Avengers',
-        'Comics',
-        'Captain America',
-        'Black Panther',
-      ]
-    },
-    {
-      name: 'Star Wars',
-      supplement: [
-        'Episode IV',
-        'Skywalker',
-        'The Empire Strikes Back',
-        'Clone Wars',
-      ]
-    },
-    {
-      name: 'National Geographic',
-      supplement: [
-        'space',
-        'geography',
-        'history',
-        'culture',
-      ]
-    },
-    {
-      name: 'ESPN',
-      supplement: [
-        'NFL',
-        'MLB',
-        'NBA',
-        'College Football',
-      ]
-    },
-  ]
-
   const category = _sample(categories)
   const categorySlug = category.name.split(' ').join('').toLocaleLowerCase()
   const supplement = _sample(category.supplement)
