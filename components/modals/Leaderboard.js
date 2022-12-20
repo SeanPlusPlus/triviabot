c42cb91b144bf7ecd5ff147aede71233053307a4
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-20 08:50:45 -0800

    main: stub out leaderboard

---
+++ components/modals/Leaderboard.js
@@ -0,0 +1,34 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
+
+const Leaderboard = () => {
+ 
+  const {
+    modal,
+    setModal,
+  } = useContext(GlobalContext)
+
+  const handleClose= () => {
+    setModal({})
+  }
+
+  return (
+    <div className={`modal ${modal && modal.leaderboard}`}>
+      <div className="modal-box relative">
+        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-4 top-4" onClick={handleClose}>✕</label>
+        <h3 className="font-bold text-xl flex">
+          <span className="text-2xl pb-4">
+            Leaderboard
+          </span>
+        </h3>
+        <div>
+        </div>
+        <div className="modal-action pt-5">
+          <label htmlFor="my-modal" className="btn" onClick={handleClose}>Close</label>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Leaderboard
---
+++ pages/leaderboard.js
@@ -0,0 +1,12 @@
+import Header from '../components/header'
+import Nav from '../components/nav'
+
+export default function App() {
+  return (
+    <div className="grid-bg md:h-screen">
+      <Header />
+      <Nav />
+      <div>leaderboard</div>
+    </div>
+  )
+}
