7ef9d96bba49cb034e7d3e84bebaa9899f199918
Sean Stephenson <sean.m.stephenson@disney.com>
2022-12-21 08:46:46 -0800

    main: 105 new qs

---
+++ scripts/buildCache.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
+curl -s "http://localhost:3000/api/question?save=true" | jq ".error";
